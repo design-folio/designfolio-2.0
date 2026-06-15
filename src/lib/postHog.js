54747357fe91545df1b43f3eb9b47e4b246c6457
Aaqif Shafi <aaqifshafi@gmail.com>
2026-06-15 12:24:55 +0530

    fix: update PostHog default date to 2026-05-30

--- src/lib/postHog.js
+++ src/lib/postHog.js
@@ -3,7 +3,7 @@ import posthog from 'posthog-js';
 if (typeof window !== 'undefined' && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    defaults: '2026-01-30',
+    defaults: '2026-05-30',
   });
 
   // To differentiate environment

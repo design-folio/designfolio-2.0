ddfa83f2cfe246f4d9b36d1d3533c61af4a11d2c
Aaqif Shafi <aaqifshafi@gmail.com>
2026-06-15 11:43:25 +0530

    fix: update PostHog configuration for session recording

--- src/lib/postHog.js
+++ src/lib/postHog.js
@@ -3,9 +3,10 @@ import posthog from 'posthog-js';
 if (typeof window !== 'undefined' && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    defaults: '2026-01-30',
     capture_pageview: true,
-    disable_session_recording: false,
+    session_recording: {
+      maskAllInputs: false,
+    },
   });
 
   // To differentiate environment
